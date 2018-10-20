dfb831e82662b28857e6147e03305b2d2f185551
Modosip <skrochinsky@gmail.com>
2018-10-20 20:05:49 +0300

    ok

--- Doggy.js
+++ Doggy.js
@@ -1,12 +1,12 @@
-bark("rover", 23);
-bark("spot", 13);
+bark("rover", "23");
+bark("spot", 13, 0);
 bark("spike", 52);
 bark("lady", 17);
 
 function bark(name, weight) {
     if (weight > 20) {
-        console.log(name + "says WOOF WOOF")
+        console.log(name + " says WOOF WOOF")
     } else {
-        console.log(name + "says woof woof")
+        console.log(name + " says woof woof")
     }
 }
